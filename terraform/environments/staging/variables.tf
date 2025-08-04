dcfea4822b970987d85e8c307e1c3546b38ec01d
prashantt597 <prashanttakkalaki1@gmail.com>
2025-08-04 12:02:54 +0530

    modified:   terraform/environments/staging/variables.tf

--- terraform/environments/staging/variables.tf
+++ terraform/environments/staging/variables.tf
@@ -15,25 +15,3 @@ variable "project_name" {
   type        = string
   default     = "flask-app"
 }
-
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.1.0.0/16"
-}
-
-variable "kubernetes_version" {
-  description = "Kubernetes version"
-  type        = string
-  default     = "1.28"
-}
-
-variable "additional_tags" {
-  description = "Additional tags to apply to resources"
-  type        = map(string)
-  default = {
-    Owner       = "DevOps-Colony"
-    CostCenter  = "Engineering"
-    Backup      = "Required"
-  }
-}
